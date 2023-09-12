71472a60067a281e95ad0b6d7109fdab6c3f5654
EvanCode20 <evan.teague11@gmail.com>
2023-09-12 07:36:48 +0000

    project creation

---
+++ PROG_ICE3/Models/Crop.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG_ICE3.Models
+{
+    public class Crop
+    {
+        public string Name { get; set; }
+        public int Yield { get; set; }
+        public double Expenses { get; set; }
+        public double Income { get; set; }
+    }
+}
